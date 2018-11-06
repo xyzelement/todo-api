70f4352623fe12476271a246502b64f7cf017e75
xyzelement <ed.markovich@gmail.com>
2018-11-06 15:53:29 -0500

    Get the api server to also serve the built react app. and add heroku hook to actually build it

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 mongoose.connect(
   "mongodb://localhost/TODO",
   { useNewUrlParser: true }
@@ -14,6 +15,8 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/users", require("./routes/users"));
+
+app.use("/", express.static(__dirname + "/../client/build"));
 
 if (require.main === module) {
   const port = process.env.PORT || 5000;

821aee4d3281adb0ed338f9799cf015722004bf8
Ed <ed@ed.com>
2018-11-10 16:52:12 -0500

    Made sprint controller testable and added tests

--- server/controllers/sprint_controller.js
+++ server/controllers/sprint_controller.js
@@ -1,27 +1,28 @@
 const Sprint = require("../models/sprint");
 
 module.exports = {
-  sprints: async (req, res, next) => {
+  sprints: async (req, res) => {
     const email = req.user.email;
     const sprints = await Sprint.find({ email });
     res.json({ sprints });
   },
-  addSprint: async (req, res, next) => {
-    const newSprint = new Sprint({
+
+  addSprint: async (req, res) => {
+    const out = await Sprint.create({
       email: req.user.email,
-      start: Date(),
+      start: new Date(),
       end: undefined
     });
-    await newSprint.save();
-    res.json({ saved: newSprint });
+
+    res.json({ saved: out });
   },
 
-  stopSprint: async (req, res, next) => {
+  stopSprint: async (req, res) => {
     const email = req.user.email;
     const _id = req.value.body.id;
-    const done = Date();
-    //TODO: error checking?
-    const result = await Sprint.updateOne({ email, _id }, { end: done });
-    return res.json({ end: done, result });
+    const done = new Date();
+
+    const out = await Sprint.findOneAndUpdate({ email, _id }, { end: done });
+    res.json({ end: done });
   }
 };
